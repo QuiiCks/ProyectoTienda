999c5e74fdcfe9671bd1b58bd1ee312f6c9b8fed
Javi <javieruizanaya@gmail.com>
2019-04-09 21:13:31 +0200

    Errores

--- src/Controladores/usuarios.java
+++ src/Controladores/usuarios.java
@@ -9,6 +9,9 @@ import java.sql.Statement;
 
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import Aplicacion.MenuTiendaAdmin;
 
 public class usuarios {
 	public static Connection conexionBBDD() throws SQLException {
@@ -40,25 +43,43 @@ public class usuarios {
 
 		Statement consulta = conexionBBDD().createStatement();
 		int resultado;
-		
-		PreparedStatement consultaprep = conexionBBDD().prepareStatement(
-				"UPDATE Users SET Rol = ? WHERE User = ?");
+
+		PreparedStatement consultaprep = conexionBBDD().prepareStatement("UPDATE Users SET Rol = ? WHERE User = ?");
 		consultaprep.setString(1, rol);
 		consultaprep.setString(2, usuario);
 		resultado = consultaprep.executeUpdate();
 		JOptionPane.showMessageDialog(null, "Usuario modificado correctamente", "Usuario modificadp", 1);
 	}
+
 	public static void eliminarUsuario(JComboBox usuarioscomboBox, JComboBox rolcomboBox) throws SQLException {
 		String usuario = usuarioscomboBox.getSelectedItem().toString();
 		String rol = rolcomboBox.getSelectedItem().toString();
 
 		Statement consulta = conexionBBDD().createStatement();
 		int resultado;
-		
-		PreparedStatement consultaprep = conexionBBDD().prepareStatement(
-				"DELETE FROM Users WHERE User = ?");
+
+		PreparedStatement consultaprep = conexionBBDD().prepareStatement("DELETE FROM Users WHERE User = ?");
 		consultaprep.setString(1, usuario);
 		resultado = consultaprep.executeUpdate();
-		JOptionPane.showMessageDialog(null, "El usuario"+ usuario+" se ha eliminado de la base de datos", "Usuario eliminado", 1);
+		JOptionPane.showMessageDialog(null, "El usuario" + usuario + " se ha eliminado de la base de datos",
+				"Usuario eliminado", 1);
+	}
+
+	public static boolean comprobarRol(JTextField textRol) throws SQLException {
+		String Rol = textRol.getText();
+
+		Statement consulta = conexionBBDD().createStatement();
+		ResultSet resultado;
+		// CREAMOS UNA CONSULTA PREPARADA PARA BUSCAR EL USUARIO ESPECIFICO
+		PreparedStatement consultaprep = conexionBBDD().prepareStatement("SELECT * FROM users WHERE user = ?");
+		consultaprep.setString(1, Rol);
+		resultado = consultaprep.executeQuery();
+
+		if (resultado.next()) {
+			if (resultado.getString("rol").equals("Admin")) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
